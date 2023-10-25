1dfa709b3ce2dfc49b327d8f50f8a8fb180eb1b1
chuong777 <chuong777@gmail.com>
2023-10-25 13:31:49 +0700

    implement

--- scripts/npm-postinstall.sh
+++ scripts/npm-postinstall.sh
@@ -3,4 +3,6 @@
 if [[ -z "${SKIP_COMPILE_PROTO}" ]]; then
   git submodule update
   bash ./scripts/compile-protoc.sh
+else
+  echo "Skip compiling proto files..."
 fi
--- scripts/npm-preinstall.sh
+++ scripts/npm-preinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "nhc test"
-
-echo "SKIP_COMPILE_PROTO = ${SKIP_COMPILE_PROTO}"
-
 if [[ -z "${SKIP_COMPILE_PROTO}" ]]; then
   git submodule init
 fi
